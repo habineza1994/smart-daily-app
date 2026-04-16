89fe3c448d4c64f22fa5161de5c8230f56242651
habineza1994 <habinezaf2050@gmail.com>
2026-04-16 12:05:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -247,4 +247,8 @@ def add_task():
     return redirect('/')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
