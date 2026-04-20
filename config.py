1bc25226b6dfc88657998bea797b6af3ba4c41b7
habineza1994 <habinezaf2050@gmail.com>
2026-04-21 01:10:49 +0200

    fix mysql database selection

--- config.py
+++ config.py
@@ -4,5 +4,6 @@ MYSQL_HOST = os.environ.get("MYSQLHOST")
 MYSQL_USER = os.environ.get("MYSQLUSER")
 MYSQL_PASSWORD = os.environ.get("MYSQLPASSWORD")
 MYSQL_DB = os.environ.get("MYSQLDATABASE")
+MYSQL_PORT = int(os.environ.get("MYSQLPORT", 3306))
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
