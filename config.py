d9deb89ddb9f367a26ef0837c98954797a4e93b7
habineza1994 <habinezaf2050@gmail.com>
2026-04-20 19:47:15 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
-MYSQL_HOST = "mysql.railway.internal"
-MYSQL_USER = "root"
-MYSQL_PASSWORD = "BYyWXYSsWIGOAKxYPBwFZEUKRFJRPytk"
-MYSQL_DB = "railway"
-SECRET_KEY = "hirwa-secret-key"
+import os
+
+MYSQL_HOST = os.environ.get("MYSQLHOST")
+MYSQL_USER = os.environ.get("MYSQLUSER")
+MYSQL_PASSWORD = os.environ.get("MYSQLPASSWORD")
+MYSQL_DB = os.environ.get("MYSQLDATABASE")
+
+SECRET_KEY = os.environ.get("SECRET_KEY", "hirwa-secret-key")
