2939915632b9e24e067da4db3aaf89d4b4d79ce7
habineza1994 <habinezaf2050@gmail.com>
2026-04-20 21:11:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -211,5 +211,8 @@ def report():
 
 
 # ================= RUN =================
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
