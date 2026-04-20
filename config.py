8393d10d76de67466a199008f32174faf8cf994a
habineza1994 <habinezaf2050@gmail.com>
2026-04-20 20:00:40 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ MYSQL_USER = os.environ.get("MYSQLUSER")
 MYSQL_PASSWORD = os.environ.get("MYSQLPASSWORD")
 MYSQL_DB = os.environ.get("MYSQLDATABASE")
 
-SECRET_KEY = os.environ.get("SECRET_KEY", "hirwa-secret-key")
+SECRET_KEY = os.environ.get("SECRET_KEY")
