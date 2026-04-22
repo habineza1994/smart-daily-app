08f9cfc0e4bf43606325cd3d3ed0e558959581a2
habineza1994 <habinezaf2050@gmail.com>
2026-04-22 22:28:18 +0200

    add missing libraries

--- app.py
+++ app.py
@@ -747,8 +747,7 @@ def activities():
         desc = request.form["description"]
 
         cur.execute(
-INSERT INTO activities (name, description, date)
-VALUES     (name, done_by, date, desc)
+         INSERT INTO activities (name, description, date)  (name, done_by, date, desc)
         )
         conn.commit()
 
